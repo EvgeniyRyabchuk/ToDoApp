33e5710da7d4c49fb7b48421ab1201a85fd4d91d
EvgeniyRyabchuk <jeka.rubchuk@gmail.com>
2021-06-03 21:38:54 +0300

    working with default custom page placeholders

--- src/index.js
+++ src/index.js
@@ -36,6 +36,8 @@ function toggleList()
         field.append(i.elem); 
 
     document.getElementById(App.cur_focus_list).click(); 
+
+    return content.length; 
 }
 // создаёт и возвращает placeholder-подсказку 
 function getFieldPlaceHolder()
@@ -49,6 +51,29 @@ function getFieldPlaceHolder()
     div.append(span); 
     return div; 
 }
+function getMenuPlaceHolder()
+{
+    let div = document.createElement("div"); 
+    div.className = "empty-menu-pl-hold"; 
+    let span = document.createElement("span"); 
+    //span.className = "ph-msg"; 
+    span.innerText = 'Нет списков'; 
+
+    div.append(span); 
+    return div; 
+}
+function getNoEntryPlaceHolder()
+{
+    let div = document.createElement("div"); 
+    div.style.display = 'block';
+    div.className = "no-entry-pl-hold"; 
+    let span = document.createElement("span"); 
+    span.innerText = 'Нет записей'; 
+
+    div.append(span); 
+    return div; 
+}
+
 
 document.getElementById("menu").addEventListener("click", (e) =>
 {
@@ -67,8 +92,11 @@ document.getElementById("menu").addEventListener("click", (e) =>
         
         App.cur_focus_list = target.id; 
         target.disabled = true;  
-        toggleList(); 
-        document.querySelector(".empty-field-pl-hold").style.display = 'none';  
+        const entry_count = toggleList(); 
+        if(entry_count == 0)
+            document.querySelector("#field").append(getNoEntryPlaceHolder());   
+
+        //document.querySelector(".empty-field-pl-hold").style.display = 'none';  
     }
 })
 
@@ -91,20 +119,35 @@ document.getElementById("addList_btn").addEventListener("click", (e) =>
     App.addList(list); 
     
     document.querySelector('#menu').append(list.elem); 
-    document.querySelector(".empty-field-pl-hold").style.display = 'none';  
+    if(App.lists.length == 1)   
+    {
+        document.querySelector(".empty-menu-pl-hold").remove(); 
+        document.querySelector(".empty-field-pl-hold").remove();
+        document.querySelector("#field").append(getNoEntryPlaceHolder()); 
+    }
+    
+
 })
 
 document.getElementById("addEntry_btn").addEventListener("click", (e) =>
 {
+   
+
     let entry = new Entry("some text", true); 
     let curList = App.getCurList(); 
+
     curList.addEntry(entry);  
     console.log(entry.elem)
     entry.elem.addEventListener("dblclick", entrySectionClick); 
     entry.elem.addEventListener("click", del_entry); 
     entry.elem.querySelector(".check-namer").addEventListener("blur", changeEntryName); 
-
+    
+    if(curList.entryList.length == 1)
+        document.querySelector(".no-entry-pl-hold").remove(); 
+    
     document.querySelector('#field').append(entry.elem);
+    
+   
 })
 
 document.getElementById("save_btn").addEventListener("click", (e) =>
@@ -118,11 +161,10 @@ function restore()
 {
     console.dir(JSON.parse(localStorage.getItem('lastSave'))); 
     const data = JSON.parse(localStorage.getItem('lastSave')); 
-    if(data)
-    {
+    if(data) {
         //App.cur_focus_list = localStorage.getItem("lastFocus");
         // инициализирую api объекты 
-        for(let listData of data)
+        for(let listData of data) 
         {
             let restoredList = new List(listData.title); 
             restoredList.id = listData.id; 
@@ -142,8 +184,12 @@ function restore()
             restoredList.elem.disabled = false; 
             document.getElementById("menu").append(restoredList.elem); 
         }
-        //toggleList(); 
+        document.querySelector(".empty-menu-pl-hold").remove(); 
     }
+    else {
+        //document.querySelector(".menu-wrapper").append(());      
+        //ocument.querySelector(".empty-menu-pl-hold").style.display = 'flex';  
+    } 
 }
 
 document.getElementById("deleteList_btn").addEventListener("click", (e) =>
@@ -163,6 +209,7 @@ document.getElementById("deleteList_btn").addEventListener("click", (e) =>
             let field = document.getElementById("field"); 
             field.innerHTML = ''; 
             field.append(getFieldPlaceHolder()); 
+            
         }
         else
         {
@@ -224,13 +271,15 @@ function del_entry(e)
     console.log("lksjdgflsjldfjg")
     if(e.target.className == "check-delete-img")
     {
-        
         let id = e.target.dataset.for; 
-        App.getCurList().deleteEntry(id); 
+        let curList = App.getCurList();
+        curList.deleteEntry(id); 
         let elem = e.target.closest('.form-check'); 
         console.log(elem)
         elem.remove(); 
-        //elem.outerHTML = ""; 
+        
+        if(curList.entryList.length == 0)
+            document.querySelector("#field").append(getNoEntryPlaceHolder()); 
     }
 }
 
